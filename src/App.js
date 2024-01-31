ffc9011c7e171aa23b6b3580ce6c46544afbaf90
Brendan96 <Brendan.ballard96@yahoo.com>
2024-01-30 20:02:20 -0600

    replace false with showscore

--- src/App.js
+++ src/App.js
@@ -91,7 +91,7 @@ export default function App() {
 		<div className='app'>
 			{/* HINT: replace "false" with logic to display the 
       score when the user has answered all the questions */}
-			{false ? (
+			{showScore ? (
 				<div className='score-section'>You scored 1 out of {questions.length}</div>
 			) : (
 				<>
